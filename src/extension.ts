6fec7510a7f773daca1400ce349a1e79aaadd482
TomPretty <tompretty2@gmail.com>
2021-03-20 18:37:44 +0000

    Add uncomplete command + refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import { homedir } from "os";
 import { join } from "path";
 import * as vscode from "vscode";
 import { getFormattedDate } from "./date";
-import { completeTodo, format, parse } from "./todo";
+import { completeTodo, format, parse, Todo, uncompleteTodo } from "./todo";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
@@ -36,22 +36,36 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand("vs-todo.completeTodo", () => {
-      const editor = vscode.window.activeTextEditor;
-
-      if (editor) {
-        const todos = parse(editor.document.getText());
-        const currentLine = editor.selection.active.line;
-        completeTodo(todos, currentLine);
-
-        const startPosition = new vscode.Position(0, 0);
-        const endPosition = new vscode.Position(editor.document.lineCount, 0);
-        const range = new vscode.Range(startPosition, endPosition);
-        editor.edit((editBuilder) => {
-          editBuilder.replace(range, format(todos) + "\n");
-        });
-      }
+      runTodoCommand(completeTodo);
     })
   );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vs-todo.uncompleteTodo", () => {
+      runTodoCommand(uncompleteTodo);
+    })
+  );
+}
+
+function runTodoCommand(command: (todos: Todo[], currentLine: number) => void) {
+  const editor = vscode.window.activeTextEditor;
+
+  if (editor) {
+    const todos = parse(editor.document.getText());
+    const currentLine = editor.selection.active.line;
+    command(todos, currentLine);
+
+    formatFile(todos, editor);
+  }
+}
+
+function formatFile(todos: Todo[], editor: vscode.TextEditor) {
+  const startPosition = new vscode.Position(0, 0);
+  const endPosition = new vscode.Position(editor.document.lineCount, 0);
+  const range = new vscode.Range(startPosition, endPosition);
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, format(todos) + "\n");
+  });
 }
 
 export function deactivate() {}
