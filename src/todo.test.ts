7de81f4978017455d457141125480bda8e079f40
TomPretty <tompretty2@gmail.com>
2021-03-21 15:15:23 +0000

    wip

--- src/todo.test.ts
+++ src/todo.test.ts
@@ -1,277 +1,318 @@
-import {
-  completeTodo,
-  format,
-  parse,
-  toggleTodo,
-  uncompleteTodo,
-  createChildTodo,
-  createSiblingTodo,
-} from "./todo";
+import { TodoList } from "./todo";
 
-describe("parse", () => {
-  it("parses a single unchecked todo", () => {
-    const text = `
-    - [ ] a
-    `;
+describe("TodoList.addTodo", () => {
+  it("adds a todo", () => {
+    const todoList = new TodoList();
 
-    const [todo] = parse(text);
+    const id = todoList.addTodo("a", false);
 
-    expect(todo.body).toBe("a");
-    expect(todo.isComplete).toBeFalsy();
-  });
-
-  it("parses a single unchecked todo", () => {
-    const text = `
-    - [x] a
-    `;
-
-    const [todo] = parse(text);
-
-    expect(todo.body).toBe("a");
-    expect(todo.isComplete).toBeTruthy();
-  });
-
-  it("parses multiple top level todos", () => {
-    const text = `
-    - [ ] a
-    - [ ] b
-    `;
-
-    const todos = parse(text);
-
-    expect(todos.map((todo) => todo.body)).toEqual(["a", "b"]);
-  });
-
-  it("parses a nested todo", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-    `;
-
-    const [a, a1] = parse(text);
-
-    expect(a1.parentId).toBe(a.id);
-  });
-
-  it("parses multiple nested todos", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-      - [ ] a2
-    `;
-
-    const [a, a1, a2] = parse(text);
-
-    expect(a1.parentId).toBe(a.id);
-    expect(a2.parentId).toBe(a.id);
-  });
-
-  it("parses a todo after a nested todo", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-        - [ ] a1i
-      - [ ] a2
-    `;
-
-    const [a, , , a2] = parse(text);
-
-    expect(a2.parentId).toBe(a.id);
-  });
-
-  it("parses child relationships", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-      - [ ] a2
-    `;
-
-    const [a, a1, a2] = parse(text);
-
-    expect(a.childIds).toEqual([a1.id, a2.id]);
+    expect(todoList.body(id)).toBe("a");
+    expect(todoList.isComplete(id)).toBeFalsy();
   });
 });
 
-describe("completeTodo", () => {
+// describe.only("parse", () => {
+//   it.only("parses a single unchecked todo", () => {
+//     const text = `
+//     - [ ] a
+//     `;
+
+//     const { 0: todo } = parse(text);
+
+//     expect(todo.body).toBe("a");
+//     expect(todo.isComplete).toBeFalsy();
+//   });
+
+//   it("parses a single unchecked todo", () => {
+//     const text = `
+//     - [x] a
+//     `;
+
+//     const { 0: todo } = parse(text);
+
+//     expect(todo.body).toBe("a");
+//     expect(todo.isComplete).toBeTruthy();
+//   });
+
+//   it("parses multiple top level todos", () => {
+//     const text = `
+//     - [ ] a
+//     - [ ] b
+//     `;
+
+//     const { 0: a, 1: b } = parse(text);
+
+//     expect(a.body).toEqual("a");
+//     expect(a.nextId).toEqual(b.id);
+//     expect(a.prevId).toBeNull();
+
+//     expect(b.body).toEqual("b");
+//     expect(b.nextId).toBeNull();
+//     expect(b.prevId).toEqual(a.id);
+//   });
+
+//   it("parses a nested todo", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//     `;
+
+//     const { 0: a, 1: a1 } = parse(text);
+
+//     expect(a1.parentId).toBe(a.id);
+//   });
+
+//   it("parses multiple nested todos", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//       - [ ] a2
+//     `;
+
+//     const { 0: a, 1: a1, 2: a2 } = parse(text);
+
+//     expect(a2.parentId).toBe(a.id);
+//   });
+
+//   it("parses a todo after a nested todo", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//         - [ ] a1i
+//       - [ ] a2
+//     `;
+
+//     const { 0: a, 3: a2 } = parse(text);
+
+//     expect(a2.parentId).toBe(a.id);
+//   });
+
+//   it("parses child relationships", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//       - [ ] a2
+//     `;
+
+//     const { 0: a, 1: a1, 2: a2 } = parse(text);
+
+//     expect(a.childIds).toEqual([a1.id, a2.id]);
+//   });
+// });
+
+describe("TodoList.completeTodo", () => {
   it("completes a single todo", () => {
-    const text = `
-    - [ ] a
-    `;
+    const todoList = new TodoList();
+    const id = todoList.addTodo("a", false);
 
-    const [todo] = parse(text);
-    completeTodo([todo], todo.id);
+    todoList.completeTodo(id);
 
-    expect(todo.isComplete).toBeTruthy();
+    expect(todoList.isComplete(id)).toBeTruthy();
   });
 
   it("completes a todo and all its children", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-      - [ ] a2
-    `;
+    const todoList = new TodoList();
+    const pId = todoList.addTodo("p", false);
+    const c1Id = todoList.addTodo("c1", false, pId);
+    const c2Id = todoList.addTodo("c2", false, pId);
 
-    const todos = parse(text);
-    completeTodo(todos, todos[0].id);
+    todoList.completeTodo(pId);
 
-    expect(todos.every((todo) => todo.isComplete)).toBeTruthy();
+    expect(todoList.isComplete(c1Id)).toBeTruthy();
+    expect(todoList.isComplete(c2Id)).toBeTruthy();
   });
 
   it("completes a parent todo if it was the last uncompleted child", () => {
-    const text = `
-    - [ ] a
-      - [x] a1
-      - [ ] a2
-    `;
+    const todoList = new TodoList();
+    const pId = todoList.addTodo("p", false);
+    const cId = todoList.addTodo("c", false, pId);
 
-    const todos = parse(text);
-    completeTodo(todos, todos[2].id);
+    todoList.completeTodo(cId);
 
-    expect(todos[0].isComplete).toBeTruthy();
+    expect(todoList.isComplete(pId)).toBeTruthy();
+  });
+
+  describe("TodoList.uncompleteTodo", () => {
+    it("uncompletes a single todo", () => {
+      const todoList = new TodoList();
+      const id = todoList.addTodo("a", true);
+
+      todoList.uncompleteTodo(id);
+
+      expect(todoList.isComplete(id)).toBeFalsy();
+    });
+
+    it("uncompletes a todo and all its children", () => {
+      const todoList = new TodoList();
+      const pId = todoList.addTodo("p", true);
+      const c1Id = todoList.addTodo("c1", true, pId);
+      const c2Id = todoList.addTodo("c2", true, pId);
+
+      todoList.uncompleteTodo(pId);
+
+      expect(todoList.isComplete(c1Id)).toBeFalsy();
+      expect(todoList.isComplete(c2Id)).toBeFalsy();
+    });
+
+    it("uncompletes a parent todo", () => {
+      const todoList = new TodoList();
+      const pId = todoList.addTodo("p", true);
+      const cId = todoList.addTodo("c", true, pId);
+
+      todoList.uncompleteTodo(cId);
+
+      expect(todoList.isComplete(pId)).toBeFalsy();
+    });
   });
 });
 
-describe("uncompleteTodo", () => {
-  it("uncompletes a single todo", () => {
-    const text = `
-    - [x] a
-    `;
-
-    const [todo] = parse(text);
-    uncompleteTodo([todo], todo.id);
-
-    expect(todo.isComplete).toBeFalsy();
-  });
-
-  it("uncompletes a todo and all its children", () => {
-    const text = `
-    - [x] a
-      - [x] a1
-      - [x] a2
-    `;
-
-    const todos = parse(text);
-    uncompleteTodo(todos, todos[0].id);
-
-    expect(todos.map((todo) => todo.isComplete)).toEqual([false, false, false]);
-  });
-
-  it("uncompletes a parent todo", () => {
-    const text = `
-    - [x] a
-      - [x] a1
-    `;
-
-    const todos = parse(text);
-    uncompleteTodo(todos, todos[1].id);
-
-    expect(todos.map((todo) => todo.isComplete)).toEqual([false, false]);
-  });
-});
-
-describe("toggleTodo", () => {
+describe("TodoList.toggleTodo", () => {
   it("completes an uncompleted todo", () => {
-    const text = `
-    - [ ] a
-    `;
+    const todoList = new TodoList();
+    const id = todoList.addTodo("a", false);
 
-    const [todo] = parse(text);
-    toggleTodo([todo], todo.id);
+    todoList.toggleTodo(id);
 
-    expect(todo.isComplete).toBeTruthy();
+    expect(todoList.isComplete(id)).toBeTruthy();
   });
 
   it("uncompletes a completed todo", () => {
-    const text = `
-    - [x] a
-    `;
+    const todoList = new TodoList();
+    const id = todoList.addTodo("a", true);
 
-    const [todo] = parse(text);
-    toggleTodo([todo], todo.id);
+    todoList.toggleTodo(id);
 
-    expect(todo.isComplete).toBeFalsy();
+    expect(todoList.isComplete(id)).toBeFalsy();
   });
 });
 
-describe("createChildTodo", () => {
-  it("creates a child todo", () => {
-    const text = `
-    - [ ] a
-    `;
+// describe("createChildTodo", () => {
+//   it("creates a child todo", () => {
+//     const text = `
+//     - [ ] a
+//     `;
 
-    const todos = parse(text);
-    createChildTodo(todos, 0);
+//     const todos = parse(text);
+//     createChildTodo(todos, 0);
 
-    const [a, a1] = todos;
+//     const { 0: a, 1: a1 } = todos;
 
-    expect(a.childIds).toEqual([a1.id]);
-    expect(a1.parentId).toEqual(a.id);
-  });
+//     expect(a.childIds).toEqual([a1.id]);
+//     expect(a.nextId).toBe(a1.id);
 
-  it("uncompletes the parent todo", () => {
-    const text = `
-    - [x] a
-    `;
+//     expect(a1.parentId).toBe(a.id);
+//     expect(a1.prevId).toBe(a.id);
+//     expect(a1.nextId).toBeNull();
+//   });
 
-    const [a] = parse(text);
-    createChildTodo([a], a.id);
+//   it("uncompletes the parent todo", () => {
+//     const text = `
+//     - [x] a
+//     `;
 
-    expect(a.isComplete).toBeFalsy();
-  });
-});
+//     const [a] = parse(text);
+//     createChildTodo([a], a.id);
 
-describe("createSiblingTodo", () => {
-  it("creates a sibling todo", () => {
-    const text = `
-    - [ ] a
-    `;
+//     expect(a.isComplete).toBeFalsy();
+//   });
+// });
 
-    const todos = parse(text);
-    createSiblingTodo(todos, 0);
+// describe("createSiblingTodo", () => {
+//   it("creates a sibling todo", () => {
+//     const text = `
+//     - [ ] a
+//     `;
 
-    const [, b] = todos;
+//     const todos = parse(text);
+//     createSiblingTodo(todos, 0);
 
-    expect(b.parentId).toBeNull();
-  });
+//     const [, b] = todos;
 
-  it("uncompletes the parent todo", () => {
-    const text = `
-    - [x] a
-      - [x] a1
-    `;
+//     expect(b.parentId).toBeNull();
+//   });
 
-    const todos = parse(text);
-    createSiblingTodo(todos, 1);
+//   it("uncompletes the parent todo", () => {
+//     const text = `
+//     - [x] a
+//       - [x] a1
+//     `;
 
-    const [a] = todos;
+//     const todos = parse(text);
+//     createSiblingTodo(todos, 1);
 
-    expect(a.isComplete).toBeFalsy();
-  });
-});
+//     const [a] = todos;
 
-describe("format", () => {
-  it("prints a todo list", () => {
-    const text = `
-    - [x] a
-    - [ ] b
-    `;
+//     expect(a.isComplete).toBeFalsy();
+//   });
+// });
 
-    const todos = parse(text);
-    const formatted = format(todos);
+// describe("deleteTodo", () => {
+//   it("deletes a todo", () => {
+//     const text = `
+//     - [ ] a
+//     - [ ] b
+//     `;
 
-    expect(formatted).toEqual("- [x] a\n- [ ] b");
-  });
+//     const todos = parse(text);
+//     deleteTodo(todos, 0);
 
-  it("prints a todo list with nested todos", () => {
-    const text = `
-    - [ ] a
-      - [ ] a1
-        - [ ] a1i
-    `;
+//     expect(todos.length).toEqual(1);
+//     expect(todos[0].body).toEqual("b");
+//   });
 
-    const todos = parse(text);
-    const formatted = format(todos);
+//   it("deletes all nested todos", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//         - [ ] a1i
+//       - [ ] a2
+//     - [ ] b
+//     `;
 
-    expect(formatted).toEqual("- [ ] a\n  - [ ] a1\n    - [ ] a1i");
-  });
-});
+//     const todos = parse(text);
+//     deleteTodo(todos, 0);
+
+//     expect(todos.length).toEqual(1);
+//     expect(todos[0].body).toEqual("b");
+//   });
+
+//   it("completes the parent todo if it was the last uncompleted child", () => {
+//     const text = `
+//     - [ ] a
+//       - [x] a1
+//       - [ ] a2
+//     `;
+
+//     const todos = parse(text);
+//     deleteTodo(todos, 2);
+
+//     expect(todos[0].isComplete).toBeTruthy();
+//   });
+// });
+
+// describe("format", () => {
+//   it("prints a todo list", () => {
+//     const text = `
+//     - [x] a
+//     - [ ] b
+//     `;
+
+//     const todos = parse(text);
+//     const formatted = format(todos);
+
+//     expect(formatted).toEqual("- [x] a\n- [ ] b");
+//   });
+
+//   it("prints a todo list with nested todos", () => {
+//     const text = `
+//     - [ ] a
+//       - [ ] a1
+//         - [ ] a1i
+//     `;
+
+//     const todos = parse(text);
+//     const formatted = format(todos);
+
+//     expect(formatted).toEqual("- [ ] a\n  - [ ] a1\n    - [ ] a1i");
+//   });
+// });
--- src/todo.ts
+++ src/todo.ts
@@ -1,172 +1,267 @@
-export interface Todo {
+interface Todo {
   id: number;
+  // tree relationship
   parentId: number | null;
   childIds: number[];
+  // double linked list
+  nextId: number | null;
+  prevId: number | null;
+  // data
   body: string;
   isComplete: boolean;
 }
 
-const TODO_REGEX = /- \[( |x)\] (.*)/;
+interface Todos {
+  [id: number]: Todo;
+}
 
-export function parse(text: string): Todo[] {
-  const lines = text.split("\n");
+export class TodoList {
+  _todos: Todos = {};
+  _nextId = 0;
 
-  let nextId = 0;
-  const indentations: number[] = [];
-  const todos: Todo[] = [];
-  lines.forEach((line) => {
-    const match = line.match(TODO_REGEX);
-    if (!match) {
-      return;
-    }
-    const [, check, body] = match;
-    const isComplete = check === "x";
+  static parse(text: string): TodoList {
+    const todoList = new TodoList();
 
-    const indentation = getIndentation(line);
-    let parentId: number | null = null;
-    if (todos.length === 0) {
-      parentId = null;
-    } else if (indentation > indentations[indentations.length - 1]) {
-      parentId = todos[todos.length - 1].id;
-    } else {
-      for (let i = indentations.length - 1; i > -1; i--) {
-        if (indentation === indentations[i]) {
-          parentId = todos[i].parentId;
-          break;
-        }
+    return todoList;
+  }
+
+  addTodo(body: string, isComplete: boolean, parentId?: number): number {
+    const newTodo: Todo = {
+      id: this._nextId++,
+      parentId: null,
+      childIds: [],
+      nextId: null,
+      prevId: null,
+      body,
+      isComplete,
+    };
+
+    if (parentId !== undefined) {
+      const parentTodo = this._todos[parentId];
+
+      // wire up tree relationships
+      newTodo.parentId = parentId;
+      parentTodo.childIds.push(newTodo.id);
+
+      // wire up linked list relationships
+      if (parentTodo.nextId) {
+        const nextTodo = this._todos[parentTodo.nextId];
+
+        parentTodo.nextId = newTodo.id;
+        newTodo.prevId = parentTodo.id;
+        newTodo.nextId = nextTodo.id;
+        nextTodo.prevId = newTodo.id;
       }
     }
-    indentations.push(indentation);
 
-    todos.push({ id: nextId++, parentId, childIds: [], body, isComplete });
-  });
+    this._todos[newTodo.id] = newTodo;
 
-  todos.forEach((todo) => {
-    if (todo.parentId !== null) {
-      todos[todo.parentId].childIds.push(todo.id);
+    return newTodo.id;
+  }
+
+  completeTodo(
+    id: number,
+    recurseDown: boolean = true,
+    recurseUp: boolean = true
+  ): void {
+    const todo = this._todos[id];
+    todo.isComplete = true;
+
+    if (recurseDown) {
+      todo.childIds.forEach((childId) =>
+        this.completeTodo(childId, true, false)
+      );
     }
-  });
 
-  return todos;
-}
-
-function getIndentation(line: string): number {
-  const match = line.match(/^(\s*)/);
-  if (!match) {
-    return 0;
-  }
-  return match[1].length;
-}
-
-export function completeTodo(
-  todos: Todo[],
-  id: number,
-  recurseDown: boolean = true,
-  recurseUp: boolean = true
-) {
-  const todo = todos[id];
-  todo.isComplete = true;
-
-  if (recurseDown) {
-    todo.childIds.forEach((childId) =>
-      completeTodo(todos, childId, true, false)
-    );
-  }
-
-  if (recurseUp && todo.parentId !== null) {
-    const parent = todos[todo.parentId];
-    if (parent.childIds.every((childId) => todos[childId].isComplete)) {
-      completeTodo(todos, parent.id, false, true);
+    if (recurseUp && todo.parentId !== null) {
+      const parent = this._todos[todo.parentId];
+      if (parent.childIds.every((childId) => this._todos[childId].isComplete)) {
+        this.completeTodo(parent.id, false, true);
+      }
     }
   }
-}
 
-export function uncompleteTodo(
-  todos: Todo[],
-  id: number,
-  recurseDown: boolean = true,
-  recurseUp: boolean = true
-) {
-  const todo = todos[id];
-  todo.isComplete = false;
+  uncompleteTodo(
+    id: number,
+    recurseDown: boolean = true,
+    recurseUp: boolean = true
+  ): void {
+    const todo = this._todos[id];
+    todo.isComplete = false;
 
-  if (recurseDown) {
-    todo.childIds.forEach((childId) =>
-      uncompleteTodo(todos, childId, true, false)
-    );
+    if (recurseDown) {
+      todo.childIds.forEach((childId) =>
+        this.uncompleteTodo(childId, true, false)
+      );
+    }
+
+    if (recurseUp && todo.parentId !== null) {
+      this.uncompleteTodo(todo.parentId, false, true);
+    }
   }
 
-  if (recurseUp && todo.parentId !== null) {
-    uncompleteTodo(todos, todo.parentId, false, true);
+  toggleTodo(id: number): void {
+    if (this._todos[id].isComplete) {
+      this.uncompleteTodo(id);
+    } else {
+      this.completeTodo(id);
+    }
+  }
+
+  body(id: number): string {
+    return this._todos[id].body;
+  }
+
+  isComplete(id: number): boolean {
+    return this._todos[id].isComplete;
   }
 }
 
-export function toggleTodo(todos: Todo[], id: number) {
-  if (todos[id].isComplete) {
-    uncompleteTodo(todos, id);
-  } else {
-    completeTodo(todos, id);
-  }
-}
+// const TODO_REGEX = /- \[( |x)\] (.*)/;
 
-export function createChildTodo(todos: Todo[], id: number) {
-  const todo = todos[id];
-  const child: Todo = {
-    id: todos.length,
-    parentId: todo.id,
-    childIds: [],
-    isComplete: false,
-    body: "",
-  };
-  todo.childIds = [child.id, ...todo.childIds];
-  todos.push(child);
-  uncompleteTodo(todos, id, false, true);
-}
+// export function parse(text: string): Todos {
+//   const lines = text.split("\n");
 
-export function createSiblingTodo(todos: Todo[], id: number) {
-  const todo = todos[id];
-  const sibling: Todo = {
-    id: todos.length,
-    parentId: todo.parentId,
-    childIds: [],
-    isComplete: false,
-    body: "",
-  };
-  if (todo.parentId !== null) {
-    const parent = todos[todo.parentId];
-    const indexOfTodo = parent.childIds.findIndex((cId) => cId === todo.id);
-    parent.childIds.splice(indexOfTodo + 1, 0, sibling.id);
-    uncompleteTodo(todos, parent.id, false, true);
-  }
-  todos.push(sibling);
+//   const todos: Todos = { nextId: 0 };
+//   let nextId = 0;
+//   const indentations: number[] = [];
+//   lines.forEach((line) => {
+//     const match = line.match(TODO_REGEX);
+//     if (!match) {
+//       return;
+//     }
+//     const [, check, body] = match;
+//     const isComplete = check === "x";
 
-  const newTodoLineNumber = getLastDescendant(todos, id) + 1;
-  return newTodoLineNumber;
-}
+//     const indentation = getIndentation(line);
+//     let parentId: number | null = null;
+//     if (nextId === 0) {
+//       parentId = null;
+//     } else if (indentation > indentations[nextId - 1]) {
+//       parentId = todos[nextId - 1].id;
+//     } else {
+//       for (let i = indentations.length - 1; i > -1; i--) {
+//         if (indentation === indentations[i]) {
+//           parentId = todos[i].parentId;
+//           break;
+//         }
+//       }
+//     }
+//     indentations.push(indentation);
 
-function getLastDescendant(todos: Todo[], id: number): number {
-  const todo = todos[id];
-  if (todo.childIds.length > 0) {
-    return getLastDescendant(todos, todo.childIds[todo.childIds.length - 1]);
-  }
-  return id;
-}
+//     todos[nextId] = {
+//       id: nextId,
+//       parentId,
+//       childIds: [],
+//       nextId: null,
+//       prevId: nextId === 0 ? null : nextId - 1,
+//       body,
+//       isComplete,
+//     };
+//     nextId++;
+//   });
 
-export function format(todos: Todo[]) {
-  return todos
-    .filter((todo) => todo.parentId === null)
-    .map((todo) => formatTodo(todos, todo))
-    .join("\n");
-}
+//   todos.nextId = nextId;
 
-const INDENTATION_PER_LEVEL = 2;
+//   Object.values(todos).forEach((todo: Todo) => {
+//     if (todo.parentId !== null) {
+//       todos[todo.parentId].childIds.push(todo.id);
+//     }
+//     if (todo.prevId !== null) {
+//       todos[todo.prevId].nextId = todo.id;
+//     }
+//   });
 
-function formatTodo(todos: Todo[], todo: Todo, level: number = 0): string {
-  const indentation = " ".repeat(INDENTATION_PER_LEVEL * level);
-  const marker = `- [${todo.isComplete ? "x" : " "}] `;
+//   return todos;
+// }
 
-  return [
-    `${indentation}${marker}${todo.body}`,
-    ...todo.childIds.map((cId) => formatTodo(todos, todos[cId], level + 1)),
-  ].join("\n");
-}
+// function getIndentation(line: string): number {
+//   const match = line.match(/^(\s*)/);
+//   if (!match) {
+//     return 0;
+//   }
+//   return match[1].length;
+// }
+
+// export function createChildTodo(todos: Todos, id: number) {
+//   const todo = todos[id];
+//   const child: Todo = {
+//     id: todos.nextId,
+//     parentId: todo.id,
+//     nextId: todo.nextId,
+//     prevId: todo.id,
+//     childIds: [],
+//     isComplete: false,
+//     body: "",
+//   };
+
+//   if (todo.nextId) {
+//     todos[todo.nextId].prevId = child.id;
+//   }
+//   todo.nextId = child.id;
+//   todo.childIds = [child.id, ...todo.childIds];
+
+//   todos[child.id] = child;
+//   todos.nextId++;
+//   uncompleteTodo(todos, id, false, true);
+// }
+
+// export function createSiblingTodo(todos: Todo[], id: number) {
+//   const todo = todos[id];
+//   const sibling: Todo = {
+//     id: todos.length,
+//     parentId: todo.parentId,
+//     childIds: [],
+//     isComplete: false,
+//     body: "",
+//   };
+//   if (todo.parentId !== null) {
+//     const parent = todos[todo.parentId];
+//     const indexOfTodo = parent.childIds.findIndex((cId) => cId === todo.id);
+//     parent.childIds.splice(indexOfTodo + 1, 0, sibling.id);
+//     uncompleteTodo(todos, parent.id, false, true);
+//   }
+//   todos.push(sibling);
+
+//   const newTodoLineNumber = getLastDescendant(todos, id) + 1;
+//   return newTodoLineNumber;
+// }
+
+// export function deleteTodo(todos: Todo[], id: number) {
+//   const index = todos.findIndex((t) => t.id === id);
+//   const todo = todos[index];
+
+//   todo.childIds.map((cId) => deleteTodo(todos, cId));
+//   todos.splice(index, 1);
+
+//   if (todo.parentId) {
+//     const parentIndex = todos.findIndex((t) => t.id === todo.parentId);
+//   }
+// }
+
+// function getLastDescendant(todos: Todo[], id: number): number {
+//   const todo = todos[id];
+//   if (todo.childIds.length > 0) {
+//     return getLastDescendant(todos, todo.childIds[todo.childIds.length - 1]);
+//   }
+//   return id;
+// }
+
+// export function format(todos: Todo[]) {
+//   return todos
+//     .filter((todo) => todo.parentId === null)
+//     .map((todo) => formatTodo(todos, todo))
+//     .join("\n");
+// }
+
+// const INDENTATION_PER_LEVEL = 2;
+
+// function formatTodo(todos: Todo[], todo: Todo, level: number = 0): string {
+//   const indentation = " ".repeat(INDENTATION_PER_LEVEL * level);
+//   const marker = `- [${todo.isComplete ? "x" : " "}] `;
+
+//   return [
+//     `${indentation}${marker}${todo.body}`,
+//     ...todo.childIds.map((cId) => formatTodo(todos, todos[cId], level + 1)),
+//   ].join("\n");
+// }
